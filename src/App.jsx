e266dbb941840f31b13122a0c32bea1377e5de77
busayolawumi <busayoolawumi3@gmail.com>
2023-03-21 12:25:18 +0100

    Frontend: "Done with navbar and hero components"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import { AiOutLineClose, AiOutlineMenu } from 'react-icons/ai'
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
+
+    const [nav, setNav] =useState(true)
+
+    const navToggleHandler = () =>{
+        setNav(!nav)
+    }
+
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-        <ul className='flex hidden'>
+        <ul className='hidden sm:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Company</li>
             <li className='p-4'>Resources</li>
             <li className='p-4'>About</li>
             <li className='p-4'>Contact</li>
         </ul>
-        <div>
-            <AiOutlineMenu size={20}/>
-        </div>
-        <div className ='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4'>
-            <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-            <ul className='uppercase'>
-                <li className='p-4'>Home</li>
-                <li className='p-4'>Company</li>
-                <li className='p-4'>Resources</li>
-                <li className='p-4'>About</li>
+        <button onClick={navToggleHandler} className={'block sm:hidden'}>
+            {!nav ? <AiOutlineClose size={20} /> :  <AiOutlineMenu size={20}/>} 
+        </button>
+        <div className={!nav ? 'fixed sm:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+            <h1 className='w-full mt-4 text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+            <ul className='uppercase p-4'>
+                <li className='p-4 border-b border-gray-600'>Home</li>
+                <li className='p-4 border-b border-gray-600'>Company</li>
+                <li className='p-4 border-b border-gray-600'>Resources</li>
+                <li className='p-4 border-b border-gray-600'>About</li>
                 <li className='p-4'>Contact</li>
             </ul>
         </div>
