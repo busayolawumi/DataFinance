c4a56b5489589bf39c09a593e1197c2dacf9fc2c
busayolawumi <busayoolawumi3@gmail.com>
2023-03-24 10:58:23 +0100

    Frontend: "Changed name to Data Finance"

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <h1 className='w-full text-2xl font-bold text-[#00df9a]'>Data Finance</h1>
         <ul className='hidden sm:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Company</li>
@@ -23,7 +23,7 @@ const Navbar = () => {
             {nav ? <AiOutlineClose size={20} /> :  <AiOutlineMenu size={20}/>} 
         </button>
         <div className={nav ? 'fixed sm:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] p-4 ease-in-out duration-500' : 'fixed left-[-100%]'}>
-            <h1 className='w-full mt-4 text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+            <h1 className='w-full mt-4 text-2xl font-bold text-[#00df9a]'>Data Finance</h1>
             <ul className='uppercase p-4'>
                 <li className='p-4 border-b border-gray-600'>Home</li>
                 <li className='p-4 border-b border-gray-600'>Company</li>
